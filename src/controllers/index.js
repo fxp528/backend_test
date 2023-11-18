0f63bb1e364a4ad2e3854897a2bf12f71524fb60
teng <acs104116@gm.ntcu.edu.tw>
2023-11-18 12:10:00 +0800

    修正若無上傳檔案會導致程式crash的問題

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -6,14 +6,18 @@ const upload = multer();
 
 // router.post('/import', async (req, res, next) => {
 router.post('/import', upload.single('excelFile'), async (req, res, next) => {
-  // 從請求中獲取Excel文件的Buffer
-  const buffer = req.file.buffer;
-  try {
-    const result = await importDatas(buffer);
-    return res.json(result);
-  } catch (error) {
-    return next(error);
-  }
+    // 從請求中獲取Excel文件的Buffer
+    const buffer = req.file ? req.file.buffer : null;
+    if (buffer) {
+        try {
+            const result = await importDatas(buffer);
+            return res.json(result);
+        } catch (error) {
+            return next(error);
+        }
+    } else {
+        return res.status(404).send('The file was not found.');
+    }
 });
 router.post('/count', async (req, res) => {
   try {
