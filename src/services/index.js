bca1435f516e146a00036fe11374aee2fa994391
teng <acs104116@gm.ntcu.edu.tw>
2023-11-22 16:37:52 +0800

    可通過單元測試、程式整理

--- src/services/index.js
+++ src/services/index.js
@@ -1,7 +1,5 @@
-import mongoose from 'mongoose';
 import {PerformanceRecord} from'../schema/performanceRecord.js'
-import XLSX from 'xlsx';
-import { format, parseISO } from 'date-fns';
+
 /**
  * 
  * @param { { 
@@ -16,34 +14,11 @@ import { format, parseISO } from 'date-fns';
  */
 
 async function importDatas(dto) {
-    // console.log('Buffer.isBuffer(dto)' + Buffer.isBuffer(dto))
     let data;
-    if (Buffer.isBuffer(dto)) {
-        const workbook = XLSX.read(dto, { type: 'buffer' });
-        // 獲取第一個工作表(目前指定)
-        const firstSheetName = '11200';
-        // const firstSheetName = workbook.SheetNames[0];
-        // console.log('firstSheetName:' + firstSheetName);
-        const worksheet = workbook.Sheets[firstSheetName];
-        // console.log('worksheet:' + JSON.stringify(worksheet));
-
-        // 將工作表數據轉換為JSON對象
-        data = XLSX.utils.sheet_to_json(worksheet, {
-            blankrows: false,
-            // sheetRows: 1,
-        });
-        for (let i = 0; i < data.length; i++) {
-            const dateObject = new Date((data[i].日期 - 25569) * 86400 * 1000);
-
-            // 使用 date-fns格式化日期
-            data[i].日期 = format(dateObject, 'yyyy-MM-dd');
-        }
-    } else if (Array.isArray(dto)) {
+    if (Array.isArray(dto)) {
         data = dto;
     }
 
-    // console.log('dto:' + JSON.stringify(dto));
-    // console.log('data[1]:' + JSON.stringify(data[1]));
     for (let i = 0; i < data.length; i++) {
         const performanceRecord = new PerformanceRecord({
             date: data[i].日期 ? data[i].日期 : data[i].date,
@@ -56,15 +31,11 @@ async function importDatas(dto) {
 
         try {
             const result = await performanceRecord.save();
-            // console.log('result: ' + result);
         } catch (error) {
             return error;
         }
     }
-    // console.log('formattedData:' + JSON.stringify(formattedData));
     return { success: true };
-
-    // throw 'not implement'
 }
 
 /**
@@ -83,9 +54,7 @@ async function fetchCount(dto) {
 
     let query = {};
     const startDate = new Date(dto.start);
-    // 將結束日期加一天以解決最後一天無法被正確搜尋到的問題
     let endDate = new Date(dto.end);
-    endDate.setDate(endDate.getDate() + 1);
     query.date = {
         $gte: startDate,
         $lte: endDate,
@@ -93,35 +62,28 @@ async function fetchCount(dto) {
 
     //處理指定分隊
     if (dto.dept) {
-        // 如果 dto.dept 是Array
         if (Array.isArray(dto.dept)) {
             query.dept = {
                 $in: dto.dept,
             };
         } else {
-            // 如果 dto.dept 是單一值
             query.dept = dto.dept;
         }
     }
 
     //處理指定人物
     if (dto.user) {
-        // 如果 dto.user 是Array
         if (Array.isArray(dto.user)) {
             query.user = {
                 $in: dto.user,
             };
         } else {
-            // 如果 dto.user 是單一值
             query.user = dto.user;
         }
     }
-    const performanceRecord = await PerformanceRecord.find(query);
-    // console.log("query: " + JSON.stringify(query));
-    // console.log("performanceRecord:" + performance)
+    const performanceRecordCount = await PerformanceRecord.countDocuments(query);
 
-    return Object.keys(performanceRecord).length;
-    //   throw 'not implement'
+    return performanceRecordCount;
 }
 
 /**
@@ -163,8 +125,6 @@ async function fetchList(dto) {
     }
     if (dto.end) {
         let endDate = new Date(dto.end);
-        // 將結束日期加一天以解決最後一天無法被正確搜尋到的問題
-        endDate.setDate(endDate.getDate() + 1);
         query.date = {
             ...query.date,
             $lte: endDate,
@@ -173,26 +133,22 @@ async function fetchList(dto) {
 
     //處理指定分隊
     if (dto.dept) {
-        // 如果 dto.dept 是Array
         if (Array.isArray(dto.dept)) {
             query.dept = {
                 $in: dto.dept,
             };
         } else {
-            // 如果 dto.dept 是單一值
             query.dept = dto.dept;
         }
     }
 
     //處理指定人物
     if (dto.user) {
-        // 如果 dto.user 是Array
         if (Array.isArray(dto.user)) {
             query.user = {
                 $in: dto.user,
             };
         } else {
-            // 如果 dto.user 是單一值
             query.user = dto.user;
         }
     }
@@ -203,7 +159,6 @@ async function fetchList(dto) {
         .limit(pageSize)
         .select('-__v -_id');
     return performanceRecord;
-//   throw 'not implement'
 }
 
 export { importDatas, fetchCount, fetchList };
